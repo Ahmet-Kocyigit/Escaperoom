b6f2cafcb468d0e8c43af8971e1818cea870ff59
RobinTack <robintack2505@gmail.com>
2018-12-03 16:41:36 +0100

    Edited pressable script to rotate switch

--- EscapeRoom/Assets/Pressable.cs
+++ EscapeRoom/Assets/Pressable.cs
@@ -16,6 +16,12 @@ public class Pressable : MonoBehaviour {
 
 	void OnCollisionEnter(Collision collision){
 		print(gameObject.name + " COLLISION DETECTED");
-        gameObject.transform.rotation.Set(-1.0f * gameObject.transform.rotation.x, gameObject.transform.rotation.y, gameObject.transform.rotation.z, gameObject.transform.rotation.w);
+	    if (collision.gameObject.name == "switch")
+	    {
+	        var rotationVector = transform.rotation.eulerAngles;
+	        rotationVector.x = transform.rotation.x * -1;
+	        transform.rotation = Quaternion.Euler(rotationVector);
+	    }
+	    //gameObject.transform.rotation.Set(-1.0f * gameObject.transform.rotation.x, gameObject.transform.rotation.y, gameObject.transform.rotation.z, gameObject.transform.rotation.w);
 	}
 }
