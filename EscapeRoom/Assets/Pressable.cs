9ead7789fc3a9e87627c635759edcc136557e9f9
RobinTack <robintack2505@gmail.com>
2018-12-03 15:30:43 +0100

    Added basic collision for switch

---
+++ EscapeRoom/Assets/Pressable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pressable : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print("The switch has loaded");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision collision){
+		print(gameObject.name + " COLLISION DETECTED");
+	}
+}
