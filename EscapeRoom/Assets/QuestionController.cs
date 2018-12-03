f69c9ae4e3974e465fcdf3c801032ab0d39d6555
DriesVerbeek <Dries.verbeek@gmail.com>
2018-12-03 15:31:55 +0100

    added the consoles

--- EscapeRoom/Assets/QuestionController.cs
+++ EscapeRoom/Assets/QuestionController.cs
@@ -1,19 +1,30 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using Object = System.Object;
 
 public class QuestionController : MonoBehaviour {
 
 	// Use this for initialization
     private String[] questions = new []{"0110 1000 + 0010 0011","0000 0000 + 0000 0000", "0000 0000 + 0000 0000"};
-    private int[] solutions = new[] {56, 64, 80};
+    private int[] solutions1 = new[] {56, 64, 80};
     private int currentQuestion;
 
-	void Start ()
+    public  GameObject console1;
+    public GameObject console2;
+    public GameObject console3;
+
+    void Start ()
 	{
 	    currentQuestion = 0;
-	    GetComponent<TextMesh>().text = questions[currentQuestion];
+
+	    GameObject.Find("console").GetComponent<TextMeshPro>().text = solutions1[0].ToString();
+	    GameObject.Find("console2").GetComponent<TextMeshPro>().text = solutions1[1].ToString();
+	    GameObject.Find("console3").GetComponent<TextMeshPro>().text = solutions1[2].ToString();
+
+        GetComponent<TextMesh>().text = questions[currentQuestion];
     }
 	
 	// Update is called once per frame
