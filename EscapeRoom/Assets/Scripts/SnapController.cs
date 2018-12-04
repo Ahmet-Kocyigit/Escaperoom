07939a85900f74c24fde18e3f6382e6ca93fbe93
Lukas Coomans <11601390@student.pxl.be>
2018-12-04 12:09:43 +0100

    added a scanning feature for items

---
+++ EscapeRoom/Assets/Scripts/SnapController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class SnapController : MonoBehaviour
+{
+    public GameObject displayscreen;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<ValueController>() != null)
+        {
+            var text =collision.gameObject.GetComponent<ValueController>().getvalue();
+            displayscreen.GetComponent<TextMesh>().text = text;
+        }
+    }
+}
---
+++ EscapeRoom/Assets/Scripts/ValueController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ValueController : MonoBehaviour
+{
+
+    public string value;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public string getvalue()
+    {
+        return value;
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
