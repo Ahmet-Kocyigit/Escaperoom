dadb300b8b23c6c525334cabd22c2c3b226b34f4
RobinTack <robintack2505@gmail.com>
2018-12-04 10:13:04 +0100

    Added ambient light on switch click

--- EscapeRoom/Assets/Pressable.cs
+++ EscapeRoom/Assets/Pressable.cs
@@ -8,10 +8,13 @@ public class Pressable : MonoBehaviour {
     private int max_time = 1;
     private float timer = 0f;
     private bool CanPress = true;
+    private bool IsOn = false;
 
     // Use this for initialization
-    void Start () {
-	}
+    void Start ()
+    {
+        RenderSettings.ambientLight = Color.white;
+    }
 
     // Update is called once per frame
     void Update() {
@@ -30,11 +33,27 @@ public class Pressable : MonoBehaviour {
     void OnCollisionEnter(Collision collision){
         if (CanPress)
         {
-            if (SteamVR_Input.__actions_default_in_GrabPinch.GetStateDown(SteamVR_Input_Sources.Any))
+            if (!IsOn)
             {
-                transform.Rotate(180, 0, 0);
-                CanPress = false;
+                if (SteamVR_Input.__actions_default_in_GrabPinch.GetStateDown(SteamVR_Input_Sources.Any))
+                {
+                    transform.Rotate(180, 0, 0);
+                    CanPress = false;
+                    IsOn = true;
+                    RenderSettings.ambientLight = Color.magenta;
+                }
             }
+            else
+            {
+                if (SteamVR_Input.__actions_default_in_GrabPinch.GetStateDown(SteamVR_Input_Sources.Any))
+                {
+                    transform.Rotate(180, 0, 0);
+                    CanPress = false;
+                    IsOn = false;
+                    RenderSettings.ambientLight = Color.white;
+                }
+            }
+           
         }
         
     }
